95d5b696f04a630169e52764720164f35bba5f12
MatteoNeri99 <162989922+MatteoNeri99@users.noreply.github.com>
2024-07-31 18:57:26 +0200

    Initial commit

---
+++ resources/views/admin/project/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="col-12 container p-5">
+    <a href="{{ route('admin.projects.create') }}" class="btn btn-secondary text-center">aggiungi un progetto</a>
+
+    <table class="table table-hover">
+        <thead>
+            <tr>
+            <th scope="col">Id</th>
+            <th scope="col">titolo</th>
+            <th scope="col">tipo</th>
+            <th scope="col">date</th>
+            <th scope="col">Azioni</th>
+            </tr>
+        </thead>
+
+        @foreach ($projects as $project)
+            <tbody>
+                <td>{{$project->id}}</td>
+                <td>{{$project->title}}</td>
+                @if ($project->type)
+
+                  <td>{{$project->type->name}}</td>
+
+                @else
+
+                    <td>----</td>
+
+                @endif
+
+                <td>{{$project->date}}</td>
+
+                <td> <a href="{{ Route('admin.projects.show', $project) }}" class="btn btn-primary">Scopri di più</a> </td>
+                 <td> <a href="{{ Route('admin.projects.edit', $project) }}" class="btn btn-success">Modifica</a> </td>
+                <td><form action="{{ Route('admin.projects.destroy', $project) }}"  method="POSt" class="d-inline-block">
+
+                    @method('DELETE')
+                    @csrf
+                    <button type="submit" class="btn btn-warning">Elimina</button>
+
+                </form></td>
+        @endforeach
+
+            </tbody>
+
+
+
+    </table>
+
+</div>
+
+@endsection
+
+
---
+++ resources/views/layouts/form-layout.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+<h1 class="text-center fw-bolder p-5 ">@yield('titolo')</h1>
+
+<div class="container">
+    <div class="row justify-center">
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error )
+
+                    <li>{{ $error }}</li>
+
+                @endforeach
+            </ul>
+        </div>
+
+        @endif
+        <form action="@yield('route')" method="POST">
+            @yield('method')
+
+            @csrf
+
+            <div class="mb-3">
+
+              <label for="title" class="form-label">title</label>
+
+              <input type="text" class="form-control" id="title" aria-describedby="title" name="title" value="{{ old('title',$project->title) }}">
+
+            </div>
+
+            <div class="mb-3">
+
+                <select class="form-select" aria-label="Defaul select example" name="type_id" id="type_id">
+                    @foreach ($types as $type)
+
+                        <option value="{{$type->id }}"
+
+                            {{$type->id == old('type_id' , $project->type_id) ? 'selected' : ''}}
+
+                            >{{ $type->name }}
+
+                        </option>
+
+                    @endforeach
+
+                </select>
+
+
+            </div>
+
+
+
+            <div class="mb-3">
+
+                <label for="date" class="form-label">date</label>
+
+                <input type="dateTime" class="form-control" id="date" aria-describedby="date" name="date" value="{{ old('date', $project->date) }}">
+
+              </div>
+
+
+
+            <div class="mb-3">
+
+                <label for="date" class="form-label">Descrizione</label>
+
+                <input type="description" class="form-control" id="description" aria-describedby="description" name="description" value="{{ old('description',$project->description) }}">
+
+              </div>
+
+
+              <div class="mb-3">
+
+                <label for="specie" class="form-label">Img</label>
+
+                <input type="text" class="form-control" id="image" aria-describedby="image" name="image" value="{{ old('image',$project->image) }}">
+
+              </div>
+
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+
+            <a href="{{ Route('admin.projects.index') }}" class="btn btn-secondary">Torna alla lista</a>
+        </form>
+
+    </div>
+</div>
+
+@endsection
